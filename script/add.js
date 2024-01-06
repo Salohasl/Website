9cb9974b9f8263c6345d86a051601f335110bee1
Maxim Zavidiy <zavidiy1998@mail.ru>
2024-01-06 17:26:10 +0200

    v1

---
+++ script/add.js
@@ -0,0 +1,52 @@
+//Кнопка всплытия
+const btnUp = {
+    el: document.querySelector('.btn-up'),
+    show() {
+      this.el.classList.remove('btn-up_hide');
+    },
+    hide() {
+      this.el.classList.add('btn-up_hide');
+    },
+    addEventListener() {
+      window.addEventListener('scroll', () => {
+        // определяем величину прокрутки
+        const scrollY = window.scrollY || document.documentElement.scrollTop;
+        // если страница прокручена больше чем на 400px, то  кнопка видна, иначе скрыть
+        scrollY > 400 ? this.show() : this.hide();
+      });
+      document.querySelector('.btn-up').onclick = () => {
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        });
+      }
+    }
+  }
+  btnUp.addEventListener();
+  
+  /* Для всплывающего окна */ 
+  let menuBtn = document.querySelector('.menuBtn');
+  let menu = document.querySelector('.header-nav');
+  menuBtn.addEventListener('click', () =>{
+  menuBtn.classList.toggle('active');
+  menu.classList.toggle('popUp');
+  });
+
+//Слайдер
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
